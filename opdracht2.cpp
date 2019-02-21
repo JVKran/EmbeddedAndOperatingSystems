aa143fd3439f9b4a4c3d8bb717595620f3cdd7cb
Jochem van Kranenburg <43204141+JVKran@users.noreply.github.com>
2019-02-21 13:56:55 +0100

    Opdracht afgerond

--- opdracht2.cpp
+++ opdracht2.cpp
@@ -9,32 +9,37 @@
 #include <string.h>
 
 using std::cin;
+using std::cout;
+using std::string;
+using std::endl;
 
-int main(){
-	int increment;
-	cin >> increment;
-	int fd = open("infile.txt", O_RDONLY | O_CREAT | O_TRUNC, 0644);
+int main(int argc, char* argv[]){
+	int increment = atoi(argv[1]);
+	int fd = open("infile.txt", O_RDONLY);
 	int fd1 = open("outfile.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if(fd < 0){
 		perror(" r1");
 		exit(1);
 	}
-	int i = 0;
-	int sz;
 	char *c = (char *) calloc(100, sizeof(char));
+	int infile = read(fd, c, 100);
+	string sentence = string(c);
+	int i = 0;
+	const char* characters = sentence.c_str();
+	string newSentence;
 	while(true){
-		char curChar = read(fd, c, 10);
-		const char* constCurChar = &curChar;
-		sz = write(fd1, constCurChar, strlen(constCurChar));
-		if(i + increment < 9){
-			i+=increment;
+		newSentence.push_back(characters[i]);
+		if(i + increment > 9){
+			i = i  - 9;
 		} else {
-			i = i + increment - 9;
+			i += increment;
 		}
-		if(i == 4){
+		if(i == 0){
 			break;
 		}
 	}
+	int size = write(fd1, newSentence.c_str(), strlen(newSentence.c_str()));
+	cout << characters << " naar " << newSentence << endl;
 	close(fd);
 	close(fd1);
 }
