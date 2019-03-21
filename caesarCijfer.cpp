1029fca894cd3abc7b4f622d6220dbdff2c5ee12
Jochem van Kranenburg <43204141+JVKran@users.noreply.github.com>
2019-03-21 10:09:01 +0100

    Commentaar toegevoegd en getest

--- caesarCijfer.cpp
+++ caesarCijfer.cpp
@@ -3,6 +3,8 @@
  * Om de code zo te compileren moet men het volgende doen:
  *      g++ test.cpp -o test -std=c++17
  *
+ * Gebruik als "caesarCijfer <woord>  <rotatie>" of caesarCijfer < <inputFile> <rotatie>
+ *
  */
 #include <iostream>
 #include <string>
@@ -21,7 +23,7 @@ int main(int argc, char* argv[]) {
         cerr << "Gebruik: ./caesarCijfer.exe 'BESTAND of WOORD' 'ROTATIE'" << endl;
         return 1;
     }
-    if(argc == 2 ) {
+    if(argc == 2 ) {  // Als de argument counter 2 is, moet er een bestand meegegeven zijn.
         while(!cin.eof()) {
             getline(cin, sentence);
             for (unsigned int i = 0; i < sentence.length(); i++) {
@@ -32,7 +34,7 @@ int main(int argc, char* argv[]) {
                 }
             }
         }
-    } else if(argc == 3){
+    } else if(argc == 3){ // Als de argument counter 3 is, moet er een woord meegegeven zijn
         for(unsigned int i = 0; i < string(argv[1]).length(); i++){
             if(string(argv[1])[i] != ' ' && string(argv[1])[i] != ',' && string(argv[1])[i] != '.') {
                 newSentence.push_back(char(int(string(argv[1])[i]) + stoi(string(argv[2]))));
@@ -41,6 +43,6 @@ int main(int argc, char* argv[]) {
             }
         }
     }
-    cout << newSentence;
+    cout << newSentence << endl;
     newSentence = "";
 }
